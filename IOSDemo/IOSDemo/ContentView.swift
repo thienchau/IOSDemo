0db661517ae491887214fd391c74f56ec0b7b017
Thien Chau <duythien0110@gmail.com>
2020-04-02 15:17:46 -0700

    Init project

---
+++ IOSDemo/IOSDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSDemo
+//
+//  Created by Duy Thien Chau on 4/2/20.
+//  Copyright © 2020 Duy Thien Chau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
